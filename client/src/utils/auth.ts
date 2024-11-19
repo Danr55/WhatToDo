2240a1d545313b9aab1305255229c00812e8e050
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-11-19 10:22:01 -0800

    Added authAPI post request to login route

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,8 +1,10 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
@@ -13,6 +15,14 @@ class AuthService {
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
   }
 
   getToken(): string {
